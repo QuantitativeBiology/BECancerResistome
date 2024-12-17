bf426184241bca336152c4dc558e5736e5f8c4d0
Emanuel Gonçalves <emanuelvgoncalves@gmail.com>
2024-12-17 16:40:47 +0000

    cross-reference between guides and vep

---
+++ BECancerResistome/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# Copyright (C) 2025 Emanuel Goncalves
+
+import pandas as pd
+
+
+def beagle2vep(r: pd.Series) -> list[str]:
+    """
+    Create a list of ENSEMBL VEP HGVS notations from a row of the Beagle output.
+
+    Args:
+    r: pd.Series: A row of the Beagle output. Must contain the columns "Nucleotide Edits" and "Target Transcript ID".
+
+    Returns:
+    list[str]: A list of ENSEMBL VEP HGVS notations.
+
+    """
+
+    r_edits = r["Nucleotide Edits"].split(", ")
+
+    return [f"{r['Target Transcript ID']}:c.{v}" for v in r_edits]
